f6aa8479c9b529890722c61866f6ecc1db347588
Yzzaslg <isaac.lucasg19@gmail.com>
2025-04-29 17:59:42 -0300

    AULA DIA 290425

---
+++ 29.04.25/29.04.25.py
@@ -0,0 +1,11 @@
+print('calcular área do círculo')
+
+raio = 5
+
+area = 3.14 * raio ** 2
+
+print('área do círculo:', area)
+
+print('A área do círculode raio', raio, 'é', area)
+
+print(f'a área do círculo de raio {raio} é {area:.4f}')
---
+++ 29.04.25/médias 29.05.25.py
@@ -0,0 +1,10 @@
+print('cálculo da média de 4 notas')
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+média = (nota1 + nota2 + nota3 + nota4 ) / 4
+
+print(f'a média aritmética das notas é {média:.4f}')
