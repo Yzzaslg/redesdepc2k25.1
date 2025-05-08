276da79b1c0a5afd6baefd63779c4ff04bc32cc6
Isaac Lucas 有罪 <isaac.lucasg19@gmail.com>
2025-05-08 01:57:31 -0300

    aula 06.05.25

--- 06.05.25/f2.py
+++ 06.05.25/f2.py
@@ -4,25 +4,25 @@
 import sys
 
 distancia_inicial = int(input('Digite o valor da distancia_inicial em km: '))
-if distancia_inicial <= 0
+if distancia_inicial <= 0:
     sys.exit('Informe distancia positiva')
 
 Veloc_inicial = int(input('Digite o valor da Velocidade_inicial em km/h: '))
-if Veloc_inicial <= 0
+if Veloc_inicial <= 0:
     sys.exit('Informe velocidade_inicial positiva')
 
 acele_inicial = int(input('Digite o valor da aceleracao_inicial em m/s**2: '))
-if acele_inicial <= 0
+if acele_inicial <= 0:
     sys.exit('Informe aceleracao_inicial positiva')
 
 distancia_inicial *= 1000
 Veloc_inicial /= 3.6
 
 delta = Veloc_inicial ** 2 - 4 * acele_inicial * distancia_inicial
-if delta < 0
+if delta < 0:
     sys.exit('Não é possível calcular o tempo')
 
-t = ( - Veloc_inicial + delta ** 0.5) / (2 * acele_inicial )
+t = ( -Veloc_inicial + delta ** 0.5) / (2 * acele_inicial )
 
 hora = t // 3600
 
