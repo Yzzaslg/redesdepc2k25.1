d15d98d769398366b9cf3ba548742271b2359a80
Isaac Lucas 有罪 <isaac.lucasg19@gmail.com>
2025-05-08 00:54:33 -0300

    aula 06.05.25 modificação do código

--- 06.05.25/f2.py
+++ 06.05.25/f2.py
@@ -1,19 +1,19 @@
 # PROGRAMA PARA CALCULAR TEMPO DO ESPAÇO PERCORRIDO
-# DADOS: distancia_ini ( c ) em km, veloc_inicial ( b ) em 'km/h', aceleração ( a ) em 'm/s ** 2', tempo = T em 's'
+# DADOS: distancia_ini ( c ) em km, veloc_inicial ( b ) em 'km/h', aceleração ( a ) em 'm/s ** 2', tempo = t em 's'
 
 import sys
 
 distancia_inicial = int(input('Digite o valor da distancia_inicial em km: '))
 if distancia_inicial <= 0
-    sys.exit('Informe distância positiva')
+    sys.exit('Informe distancia positiva')
 
 Veloc_inicial = int(input('Digite o valor da Velocidade_inicial em km/h: '))
 if Veloc_inicial <= 0
     sys.exit('Informe velocidade_inicial positiva')
 
-acele_inicial = int(input('Digite o valor da aceleração_inicial em m/s**2: '))
+acele_inicial = int(input('Digite o valor da aceleracao_inicial em m/s**2: '))
 if acele_inicial <= 0
-    sys.exit('Informe aceleração_inicial positiva')
+    sys.exit('Informe aceleracao_inicial positiva')
 
 distancia_inicial *= 1000
 Veloc_inicial /= 3.6
@@ -22,9 +22,14 @@ delta = Veloc_inicial ** 2 - 4 * acele_inicial * distancia_inicial
 if delta < 0
     sys.exit('Não é possível calcular o tempo')
 
+t = ( - Veloc_inicial + delta ** 0.5) / (2 * acele_inicial )
 
-1h = 60min = 3600s
+hora = t // 3600
 
-T = ( a * t ** 2 ) / 2 + v0 * t - d
+t = t % 3600
 
-D = b ** 2 - 4 * a * c
+minuto = t//60
+
+segundo = t % 60
+
+print(f'tempo = {hora} : {minuto} : {segundo}')
