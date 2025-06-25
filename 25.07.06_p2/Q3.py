5e24f30220a7e43480732240ca50e85d531f9f5e
Yzzaslg <isaac.lucasg19@gmail.com>
2025-06-25 02:41:14 -0300

    25.06.25_Q3

---
+++ 25.07.06_p2/Q3.py
@@ -0,0 +1,35 @@
+'''Na definição da Wikipédia, números triangulares são aqueles que representam o total de pontos que formam triângulos equiláteros em um plano 
+(veja a definição detalhada em https://pt.wikipedia.org/wiki/Número_triangular).
+
+Faça um programa que solicite ao usuário um número inteiro positivo e informe se ele é (ou não) triangular, de acordo com a definição.
+'''
+import sys
+
+# Solicitar o número ao usuário.
+try:
+    num = int(input('Informe um número inteiro positivo: '))
+    if num <= 0:
+        sys.exit('O Número deve ser positivo!')
+except ValueError:
+    sys.exit('Informe apenas valores reais para o número.')
+except Exception as e:
+    sys.exit(f'ERRO: {e}')
+
+# 'n' vai começar em 1.
+n = 1
+
+while n * ( n + 1 ) / 2 <= num:
+# Definição do número triangular: Tn = 1+2+3+⋯+n = n(n+1)/2.    
+    T = n * ( n + 1 ) / 2
+    print(f'T ={T}')
+    if T == num:
+        print('Achou! O número inserido é triangular.')
+        break # break vai parar o programa caso número dado pelo usuário seja igual ao triangular ( num == T ).
+    n = n + 1 # caso o número dado não seja igual ou maior ao triangular, o programa irá rodar novamente adicionando +1 a variável 'n'.
+else:
+    print('Puts! O número informado não é triangular.')
+
+print('Fim do programa, obrigado pelo teste!"-"')
+    
+    
+
