3e6dcb72095ac4297c7c58292a618c6018aa2cec
Yzzaslg <isaac.lucasg19@gmail.com>
2025-07-10 17:54:39 -0300

    25.07.10

---
+++ 25.07.10_ARQUIVOS_EXERCICIO_cotacaodolar/exercicio_cotacao_dolar.py
@@ -0,0 +1,26 @@
+'''
+   Fazer um programa que faça as seguintes operações.
+
+   1) A partir do conteúdo do arquivo cotacao_dolar.csv gerar uma lista onde cada item
+      dessa lista será uma sub-lista com os valores de cada linha do arquivo.
+
+      a) Os valores estão separados por ";";
+      b) Os dois primeiros valores são valores do tipo FLOAT;
+      c) O terceiro valor é do tipo DATE;
+      
+
+   2) Gerar arquivos para cada ano. Salvar o arquivo com o mesmo nome do arquivo que 
+      foi aberto na questão 1, adionando no final do nome o sufixo "_nnnn" onde "nnnn" 
+      corresponde ao ano das cotações;
+
+
+   3) Gerar arquivos por ano com as médias das cotações mensais. Salve os arquivos com 
+      o nome "media_cotacao_nnnn.csv" onde "nnnn" corresponde ao ano. Cada linha do arquivo
+      deverá ter o nome do mês e em seguida o valor médio da cotação. Separe os valores da 
+      linha por ";";
+'''
+
+import os, sys
+
+# Obtendo o diretório onde o programa está salvo
+strdir = os.path.dirname(__file__)
