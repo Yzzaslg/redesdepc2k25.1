2f998d048a3761d319ced0dbc0b470eab38c8eb0
Yzzaslg <isaac.lucasg19@gmail.com>
2025-07-25 18:50:41 -0300

    25.07.25_Q1

--- 27.07.25_pIII/Q1.py
+++ 27.07.25_pIII/Q1.py
@@ -6,9 +6,9 @@ import sys, random
 
 # Solicitar dados
 try:
-    n_sublistas = int(input('Informe um valor inteiro ( maior que 0 ) para a quantidade de sub-listas ( linhas ) da lista principal: '))
-    n_elementos = int(input('Informe um valor inteiro ( maior que 0 ) para a quantidade de elementos em cada sub-lista: '))
-    if n_sublistas <= 0 or n_elementos <= 0:
+    n_linhas = int(input('Informe um valor inteiro ( maior que 0 ) para a quantidade de linhas ( sub-listas ) da lista principal: '))
+    n_colunas = int(input('Informe um valor inteiro ( maior que 0 ) para a quantidade de colunas em cada sub-lista: '))
+    if n_linhas <= 0 or n_colunas <= 0:
         sys.exit('Informe um valor inteiro válido.')
 
 except ValueError:
@@ -17,18 +17,18 @@ except Exception as e:
     sys.exit(f'ERR0: {e}')
 
 # Criar a lista principal
-list_origem = [[random.randint(0, 19) for _ in range(n_elementos)] for _ in range(n_sublistas)] # random.randint vai gerar os elementos dado em () de forma aleatória, primero for in range() vai gerar uma sublista com a quantidade de elementos e o segundo for in range() repete essa sublista várias vezes.
+list_origem = [[random.randint(0, 19) for _ in range(n_colunas)] for _ in range(n_linhas)] # random.randint vai gerar os elementos dado em () de forma aleatória, primero for in range() vai gerar uma sublista com a quantidade de elementos e o segundo for in range() repete essa sublista várias vezes.
 
 
 # Criar a lista transposta
-list_inversa = [list(verticais) for verticais in zip(*list_origem)] # zip agrupa os elementos de mesma posição, formando novas colunas e o * abre uma lista e retornar cada elemento seu de forma separada.
+list_inversa = [[list_origem[l][c] for l in range(n_linhas)] for c in range(n_colunas)] # loop externo percorre as colunas da lista geral e o interna as linhas, list_origem[l (linha)][c (coluna)] acessa a posição invertida do elemento, criando a transposta.
 
 # Exibir as listas
 print('lista origem: ')
 for horizontal in list_origem:
     print(horizontal)
 
-print('\nlista inversa: ')
+print('\nlista transposta: ')
 for horizontal in list_inversa:
     print(horizontal)
 
